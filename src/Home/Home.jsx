50395c3495ff2b983b1cfea7555091efecfb970e
oo310 <cc900707@gmail.com>
2025-03-10 01:10:42 +0800

    新增公告連結

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -21,9 +21,11 @@ const Home = () => {
         <h2>平台公告</h2>
         <p>歡迎使用我們的Python學習平台！以下是一些使用指南：</p>
         <ul>
+          <li>第一次進入請點選連結<a href='https://forms.gle/D2pmjS5Pe3SjUm8U8' target="_blank">https://forms.gle/D2pmjS5Pe3SjUm8U8</a>進行前測。</li>
           <li>點擊教材列表瀏覽課程單元並選擇您感興趣的內容。</li>
           <li>選擇題目列表進行練習來加深您的知識。</li>
-          <li>請點選連結<a href='https://forms.gle/muexCsdSkbUHkCLM9'>https://forms.gle/muexCsdSkbUHkCLM9</a>告訴我們您的使用體驗，謝謝</li>
+          <li>題目練習完後請點選連結<a href='https://forms.gle/UY9z9N11qct32rHx8' target="_blank">https://forms.gle/UY9z9N11qct32rHx8</a>進行後測。</li>
+          <li>請點選連結<a href='https://forms.gle/muexCsdSkbUHkCLM9' target="_blank">https://forms.gle/muexCsdSkbUHkCLM9</a>告訴我們您的使用體驗，謝謝</li>
           <li>如果您有任何問題，請聯繫我們。</li>
         </ul>
       </div>
